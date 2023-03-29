231854c2235bcff283b3699db4b96a2325f365fa
SamuelPickering <samuelpickering21@gmail.com>
2023-03-29 18:02:35 +0000

    sixth function

--- index.js
+++ index.js
@@ -36,4 +36,10 @@ function multiply(num1 ,num2){
     return num1 * num2
 }
 
-console.log(multiply(1,5))
+//console.log(multiply(1,5))
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+console.log(add(9,9))
