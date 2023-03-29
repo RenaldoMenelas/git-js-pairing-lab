4582bd39544f73601da427bf05b889f471ed35bc
SamuelPickering <samuelpickering21@gmail.com>
2023-03-29 17:42:59 +0000

    second function

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function fiveToOneHundred(){
     }
 }
 fiveToOneHundred()
+
+function multiplesOfThree(){
+    for(let i = 3; i <= 100; i++){
+        if(i % 3 === 0){
+            console.log(i)
+        }
+    }
+}
+
+multiplesOfThree()
